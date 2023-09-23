e13daa44c463bd9c60f00fe8a421e8442874a9d4
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2023-09-23 19:00:52 +0100

    Feat: Added range

--- src/WordOrdinal.tsx
+++ src/WordOrdinal.tsx
@@ -2,33 +2,39 @@ import React from "react";
 import { convertNumbersToWords, convertTensToWords } from "./Cases";
 
 interface WordOrdinalProps {
-  number: number;
-  shouldBeTitleCase: boolean;
+	number: number;
+	shouldBeTitleCase: boolean;
 }
 
-const WordOrdinal: React.FC<WordOrdinalProps> = ({ number, shouldBeTitleCase }) => {
-  const convertNumberToOrdinal = (number: number, shouldBeTitleCase: boolean) => {
-    let position: string;
+const WordOrdinal: React.FC<WordOrdinalProps> = ({
+	number,
+	shouldBeTitleCase,
+}) => {
+	const convertNumberToOrdinal = (
+		number: number,
+		shouldBeTitleCase: boolean
+	) => {
+		let position: string;
+		if (number > 90) return "Invalid range";
+		if (number > 20 && number % 10 !== 0) {
+			const firstPosition = convertTensToWords(Math.floor(number / 10) * 10);
+			const secondPosition = `-${convertNumbersToWords(number % 10)}`;
+			position = firstPosition + secondPosition;
+		} else {
+			position = convertNumbersToWords(number);
+		}
 
-    if (number > 20 && number % 10 !== 0) {
-      const firstPosition = convertTensToWords(Math.floor(number / 10) * 10);
-      const secondPosition = `-${convertNumbersToWords(number % 10)}`;
-      position = firstPosition + secondPosition;
-    } else {
-      position = convertNumbersToWords(number);
-    }
+		return shouldBeTitleCase ? toTitleCase(position) : position;
+	};
 
-    return shouldBeTitleCase ? toTitleCase(position) : position;
-  };
-
-  return <span>{convertNumberToOrdinal(number, shouldBeTitleCase)}</span>;
+	return <span>{convertNumberToOrdinal(number, shouldBeTitleCase)}</span>;
 };
 
 const toTitleCase = (string: string) => {
-  return string
-    .split(" ")
-    .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-    .join(" ");
+	return string
+		.split(" ")
+		.map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+		.join(" ");
 };
 
 export default WordOrdinal;
